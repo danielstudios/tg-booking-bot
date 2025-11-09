1ddddb8b1e5f1910a30350d6e5cf836480e08c7b
Daniel <danielvonyavkin@gmail.com>
2025-11-10 00:40:22 +0300

    add tgapp sdk

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+import { init } from '@tma.js/sdk-vue'
 import App from './App.vue'
+
+try {
+  init()
+  console.log('Telegram WebApp API инициализирован')
+} catch (err) {
+  console.warn('Приложение запущено вне Telegram. WebApp SDK недоступен.', err)
+}
 
 createApp(App).mount('#app')
